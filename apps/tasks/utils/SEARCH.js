050eb891236d874b6cbf31124b9be09619b0b67a
vincent <kigongovincent81@gmail.com>
2024-12-05 13:19:19 +0300

    added search by department for consultants

--- apps/tasks/utils/SEARCH.js
+++ apps/tasks/utils/SEARCH.js
@@ -1,4 +1,4 @@
 export function searchItemsByName(items, query) {
     const regex = new RegExp(query?.value?.replace(/[.*+?^=!:${}()|\[\]\/\\]/g, "\\$&"), 'i'); // Escaping special characters in the query
-  return items.filter(item => regex.test(item.name));
+  return items.filter(item => regex.test(item?.name) || regex.test(item?.department));
   }
